1485d2ad16851d3d0e2d50eb3d3ba65bdf70485f
oseefotso <fotsoosee@gmail.com>
2022-11-03 20:19:55 -0400

    instance name change

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
    ami = var.AMI_ID
    instance_type = var.INTANCE_TYPE
-   tags = {Name = "airbnb-web-server"} 
+   tags = {Name = "airbnb-web-server-instance"} 
    monitoring = true
    ebs_optimized = true
    root_block_device {
