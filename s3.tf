1468c0a11bb59de2586987a9fad29af63e95fd49
oseefotso <fotsoosee@gmail.com>
2022-11-04 15:38:06 -0400

    adding s3 bucket

--- s3.tf
+++ s3.tf
@@ -1,3 +1,9 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "ball" {
   bucket = "osee12345"
   }
+  resource "aws_s3_bucket_public_access_block" "ball" {
+  bucket = aws_s3_bucket.ball.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
